02a3a3bd08fa0862e732ed7732afee9e13a6c241
Julius Lopes <juliuscxlopes@gmail.com>
2024-07-19 20:40:36 -0300

    indo a loucura

--- src/routes/webhookRoutes.js
+++ src/routes/webhookRoutes.js
@@ -1,12 +1,12 @@
 // routes/webhookRoutes.js
 const express = require('express');
 const router = express.Router();
-const handleWebhook = require('../controllers/webhookController').handleWebhook; // Modificação aqui
+const handleWebhook = require('../controllers/webhookController');
 const verificationController = require('../controllers/verificationController'); // Certifique-se de que o caminho está correto
 const webhookLogger = require('../middleware/webhookLogger'); // Importa o middleware
 
 // Rota para processar os webhooks
-router.post('/', webhookLogger, webhookController);
+router.post('/', webhookLogger, handleWebhook );
 
 // Rota para verificação do webhook
 router.get('/', verificationController.verifyWebhook);
