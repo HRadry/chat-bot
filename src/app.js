950866366571f4925bb758633214680568628801
Julius Lopes <juliuscxlopes@gmail.com>
2024-07-23 17:40:30 -0300

    pirando total

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const webhookRoutes = require('./routes/webhookRoutes');
+const redis = require('./redisClient');
 
 // Load environment variables
 dotenv.config();
@@ -13,6 +14,20 @@ app.use(express.json());
 
 // Rotas
 app.use('/webhook', webhookRoutes);
+
+const WHATSAPP_ID = '553191496778'
+app.get('/clear', async () => {
+    const contact = JSON.parse ( await redis.get(WHATSAPP_ID))
+    if (contact) {
+        console.log('foi encontrado objeto tal', contact)
+        redis.del(WHATSAPP_ID)
+    }
+    else {
+        console.log('nao foi encontrado o registro')
+    }
+
+
+})
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
