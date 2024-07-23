b2d93d006d04f358afce9e518346833d677412af
Julius Lopes <juliuscxlopes@gmail.com>
2024-07-22 23:16:48 -0300

    pirando total

--- src/routes/webhookRoutes.js
+++ src/routes/webhookRoutes.js
@@ -5,18 +5,11 @@ const { handleWebhook } = require('../controllers/webhookController');
 const verificationController = require('../controllers/verificationController');
 const messageProcessor = require('../middleware/messageProcessor');
 const statusProcessor = require('../middleware/statusProcessor');
-const contactValidationMiddleware = require('../middleware/contactValidationMiddleware');
 
 
-router.post('/', messageProcessor, statusProcessor, (req, res, next) => {
-    // Aplicar validação de CNPJ e e-mail apenas se o passo atual for relevante
-    if (req.processedData.contact.step === 'getCNPJ' || req.processedData.contact.step === 'getEmail') {
-      contactValidationMiddleware(req, res, next);
-    } else {
-      next();
-    }
-  }, handleWebhook);
-  
+// Middleware de processamento básico
+router.post('/', messageProcessor, statusProcessor, handleWebhook);
+
 // Rota para verificação do webhook
 router.get('/', verificationController.verifyWebhook);
 
